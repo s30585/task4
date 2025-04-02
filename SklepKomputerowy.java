6de486ba1f4a13499c0329cf61a4452548e1ea51
s30585 <s30585@pjwstk.edu.pl>
2025-04-02 22:40:15 +0200

    Update SklepKomputerowy.java

--- SklepKomputerowy.java
+++ SklepKomputerowy.java
@@ -1,37 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SklepKomputerowy {
-    Produkt[] produkt;
-    Klient[] klient;
-    Zamówienie[] zamowienia;
-    int liczbaProduktow;
-    int liczbaKlientow;
-    int liczbaZamowien;
-    public SklepKomputerowy(Produkt[] produkt,Klient[] klient, Zamówienie[] zamowienia, int liczbaProduktow, int liczbaKlientow, int liczbaZamowien) {
-        this.produkt = produkt;
-        this.klient = klient;
+    List<Produkt> produkty = new ArrayList<>();
+    List<Klient> klienci = new ArrayList<>();
+    List<Zamowienie> zamowienia = new ArrayList<>();
+    int liczbaProduktow = 0;
+    int liczbaKlientow = 0;
+    int liczbaZamowien = 0;
+    public SklepKomputerowy() {
         this.zamowienia = zamowienia;
         this.liczbaProduktow = liczbaProduktow;
         this.liczbaKlientow = liczbaKlientow;
         this.liczbaZamowien = liczbaZamowien;
     }
     public void dodajProdukt(Produkt produkt) {
-
+        produkty.add(produkt);
     }
     public void dodajKlienta(Klient klient) {
-
+        klienci.add(klient);
     }
-    public void utworzZamowienie(Klient[] klient, Produkt[] produkt, int[] ilosci) {
-
+    public Zamowienie utworzZamowienie(Klient klient, Produkt[] produkty, int[] ilosci) {
+        Zamowienie zamowienie = new Zamowienie(liczbaZamowien + 1, klient, produkty, ilosci, "2024-04-02", "Nowe");
+        zamowienia.add(zamowienie);
+        liczbaZamowien++;
+        return zamowienie;
     }
-    public void aktualizujStanMagazynowy(Zamówienie zamowienie) {
-        // dla każdego wyciągnietego produktu wyciadamy index ze sklepu arraylist to zrobic
+
+   public void aktualizujStanMagazynowy(Zamowienie zamowienie) {
+        for (int i = 0; i < zamowienie.produkt.length; i++) {
+           zamowienie.produkt[i].setIloscWMagazynie(zamowienie.produkt[i].getIloscWMagazynie() - zamowienie.ilosci[i]);
+        }
     }
     public void zmienStatusZamowienia(int idZamowienia, String nowyStatus) {
-
+        for (Zamowienie zamowienie : zamowienia) {
+            if (zamowienie.getId() == idZamowienia) {
+                zamowienie.setStatus(nowyStatus);
+            }
+        }
     }
     public void wyswietlProduktyWKategorii(String kategoria) {
-
+        for (Produkt produkt : produkty) {
+            if (produkt.getKategoria().equals(kategoria)) {
+                produkt.wyswietlInformacje();
+            }
+        }
     }
-    public void wyswietlZamowieniaKlienta(int idKlientA) {
-
+    public void wyswietlZamowieniaKlienta(int idKlienta) {
+        for (Zamowienie zamowienie : zamowienia) {
+            if (zamowienie.klient.getId() == idKlienta) {
+                zamowienie.wyswietlSzczegoly();
+            }
+        }
     }
 }
