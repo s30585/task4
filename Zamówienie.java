39fedfb579d68a3be7eeb0bc7f832acdefc19fee
s30585 <s30585@pjwstk.edu.pl>
2025-04-02 22:39:44 +0200

    Update Zamówienie.java

--- Zamówienie.java
+++ Zamówienie.java
@@ -1,11 +1,11 @@
-public class Zamówienie {
+public class Zamowienie {
     int id;
     Klient klient;
     Produkt[] produkt;
     int[] ilosci;
     String dataZamowienia;
     String status;
-    public Zamówienie(int id, Klient klient, Produkt[] produkt, int[] ilosci, String dataZamowienia, String status) {
+    public Zamowienie(int id, Klient klient, Produkt[] produkt, int[] ilosci, String dataZamowienia, String status) {
         this.id = id;
         this.klient = klient;
         this.produkt = produkt;
@@ -13,13 +13,32 @@ public class Zamówienie {
         this.dataZamowienia = dataZamowienia;
         this.status = status;
     }
-    public void obliczWartoscZamowienia() {
-
+    public int getId() {
+        return id;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public double obliczWartoscZamowienia() {
+        double suma = 0;
+        for (int i = 0; i < produkt.length; i++) {
+            suma += produkt[i].getCena() * ilosci[i];
+        }
+        return suma;
     }
     public void zastosujZnizke() {
-
+        if (klient.getCzyStaly()) {
+            for (int i = 0; i < produkt.length; i++) {
+                double nowaCena = produkt[i].getCena() * 0.9;
+                produkt[i].setCena(nowaCena);
+            }
+        }
     }
     public void wyswietlSzczegoly() {
-
+        System.out.println("Zamówienie ID: " + id + ", Data: " + dataZamowienia + ", Status: " + status);
+        for (int i = 0; i < produkt.length; i++) {
+            System.out.println("Produkt: " + produkt[i].getNazwa() + ", Ilość: " + ilosci[i] + ", Cena: " + produkt[i].getCena());
+        }
+        System.out.println("Łączna wartość zamówienia: " + obliczWartoscZamowienia());
     }
 }
