50511906384da66355940ee4687ed984e7de3f60
s30585 <s30585@pjwstk.edu.pl>
2025-04-02 22:39:18 +0200

    Update Klient.java

--- Klient.java
+++ Klient.java
@@ -4,14 +4,39 @@ public class Klient {
     String nazwisko;
     String email;
     boolean czyStaly;
-    public Klient(int id, String imie, String nazwisko, String email, boolean czyStaly) {
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
         this.id = id;
+    }
+    public String getImie() {
+        return imie;
+    }
+    public void setImie(String imie) {
         this.imie = imie;
+    }
+    public String getNazwisko() {
+        return nazwisko;
+    }
+    public void setNazwisko(String nazwisko) {
         this.nazwisko = nazwisko;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
         this.email = email;
+    }
+    public boolean getCzyStaly() {
+        return czyStaly;
+    }
+    public void setCzyStaly(boolean czyStaly) {
         this.czyStaly = czyStaly;
     }
-    public void wyswietlInformacje() {
 
+    public void wyswietlInformacje() {
+        System.out.println("Id: " + id + "Imie: " + imie + "Nazwisko: " + nazwisko + "Email: "+ email + "Czy jest stałym klientem: " + czyStaly);
     }
 }
