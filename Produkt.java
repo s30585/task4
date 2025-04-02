7aabea574f87230bb49f1fb8b946845b63d328c8
s30585 <s30585@pjwstk.edu.pl>
2025-04-02 22:38:03 +0200

    Update Produkt.java

--- Produkt.java
+++ Produkt.java
@@ -1,17 +1,46 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Produkt {
     int id;
     String nazwa;
     String kategoria;
     double cena;
     int iloscWMagazynie;
-    public Produkt(int id, String nazwa, String kategoria, double cena, int iloscWMagazynie) {
-        this.id = id;
-        this.nazwa = nazwa;
-        this.kategoria = kategoria;
-        this.cena = cena;
-        this.iloscWMagazynie = iloscWMagazynie;
-    }
-    public void wyswietlInformacje() {
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNazwa() {
+        return nazwa;
+    }
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+    public String getKategoria() {
+        return kategoria;
+    }
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+    public double getCena() {
+        return cena;
+    }
+    public void setCena(double cena) {
+        BigDecimal bd = new BigDecimal(cena).setScale(2, RoundingMode.HALF_UP);
+        this.cena = bd.doubleValue();
+    }
+    public int getIloscWMagazynie() {
+        return iloscWMagazynie;
+    }
+    public void setIloscWMagazynie(int ilosc) {
+        this.iloscWMagazynie = ilosc;
+    }
+
+    public void wyswietlInformacje() {
+        System.out.println("ID: "+ id +", Nazwa: " + nazwa+ ", Kategoria: " + kategoria + ", Cena: " + cena + ", Ilość w magazynie: " + iloscWMagazynie);
     }
 }
